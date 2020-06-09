bc9d8902eb3e911ecc72db97554ec215f3cf5298
xixb <xixianbing@hexiaoxiang.com>
2020-06-09 15:37:57 +0800

    mysql Demo

--- main.go
+++ main.go
@@ -20,6 +20,22 @@ func main() {
 	dbConf := viper.GetStringMapString("database")
 	resource.InitDB(dbConf["user"], dbConf["password"], dbConf["host"], dbConf["port"], dbConf["name"])
 
+   // result, _ := service.GetReviewer(6299)
+   //{6299 1 笑笑老师 https://wx.hexiaoxiang.com/miniApp/avatar/ua.jpg 1373041051 2020-03-02 10:37:01 +0800 CST 2020-03-02 10:56:04 +0800 CST <nil>}
 
 
+	//result , _:= service.SelectNameReviewer(6299)
+	//{0 1 笑笑老师   <nil> <nil> <nil>}
+
+
+
+	//_ = service.UpdateReviewer(6299)
+	// UPDATE `reviewer` SET `cellphone` = '12', `updated_at` = '2020-06-09 15:11:49'  WHERE `reviewer`.`deleted_at` IS NULL AND ((id = 6299))
+
+
+
+   // fmt.Println(result)
+
+  // _ =  service.MapUpdateReviewer(6299, map[string]interface{}{"avatar_url":"www.baidu.com","staff_id":12,"cellphone":"123"})
+ //UPDATE `reviewer` SET `avatar_url` = 'www.baidu.com', `cellphone` = '{', `staff_id` = 12, `updated_at` = '2020-06-09 15:36:23'  WHERE `reviewer`.`deleted_at` IS NULL AND ((id = 6299))
 }
--- service/reviewers.go
+++ service/reviewers.go
@@ -1,3 +1,27 @@
 package service
 
+import (
+	//"mysql/entity"
+	"mysql/model"
+	"mysql/resource"
+)
 
+func GetReviewer(id uint) (qu model.Reviewer, err error) {
+	err = resource.GetDB().Where("id = ?", id).Find(&qu).Error
+	return
+}
+
+func SelectNameReviewer(id uint) (qu model.Reviewer, err error) {
+	err = resource.GetDB().Select("name,staff_id").Where("id =?", id).Find(&qu).Error
+	return
+}
+
+func UpdateReviewer(id uint) (err error) {
+	err = resource.GetDB().Model(&model.Reviewer{}).Where("id = ?", id).Update("cellphone", "12").Error
+	return
+}
+
+func MapUpdateReviewer(id uint, m map[string]interface{})(err error){
+	err = resource.GetDB().Model(&model.Reviewer{}).Where("id = ?",id).Update(m).Error
+	return
+}
